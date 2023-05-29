966b3d48df14ef5b701335a64cd0f158aff0a5f3
Vince <marc.bentoy@gmail.com>
2023-05-29 11:21:05 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { FAQAccordion } from "@/components/faq-accordion"
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 ">
-      <div className="grid items-center gap-y-4 grid-cols-2 h-screen">
+      <div className="grid items-center gap-y-4 grid-cols-2 h-fit py-24">
         <div className="flex max-w-[980px] flex-col items-start gap-2 ">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
             Effortlessly Manage <br className="hidden sm:inline" />
